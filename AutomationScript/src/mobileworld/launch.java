db7bd1651dc4d7043dcf5a0365a83a9bab0912db
nishalini.mathavan <nishalini.mathavan@QT-INBG-LT0709.QUALITEST.LOCAL>
2022-08-02 14:54:43 +0530

    welcome page successful

--- AutomationScript/src/mobileworld/launch.java
+++ AutomationScript/src/mobileworld/launch.java
@@ -1,16 +1,25 @@
 package mobileworld;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class launch {
-	WebDriver driver;
-	@Test public void Launching_the_Browser()
+
+	@Test 
+	public void Launching_the_Browser()
 	{
-		System.setProperty("webdriver.chrome.driver",".C:\\Users\\nishalini.mathavan\\Downloads\\chromedriver_win32.exe");
-		driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver",".C:\\Users\\nishalini.mathavan\\Downloads\\chromedriver_win32.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://mobileworld.azurewebsites.net/");
 		driver.manage().window().maximize();
 				
 	}
 }
+
 
 
 
